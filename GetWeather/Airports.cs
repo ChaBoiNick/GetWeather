4598f945c95cc8c71ca34290d3b3ceb6eb019512
Nicholas Schmidt <talkschmidt@hotmail.com>
2018-08-22 16:56:29 +1000

    Fuckin...i don't know

---
+++ GetWeather/Airports.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetWeather
+{
+    class Airports
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Url {
+            get;
+            set;
+        }
+    }
+}
--- GetWeather/Program.cs
+++ GetWeather/Program.cs
@@ -15,31 +15,133 @@ namespace GetWeather
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            DateTime dateTime = DateTime.Now;
-            WebClient webClient = new WebClient();
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDQ60801/IDQ60801.94183.axf", @"C:\GetWeather\Coen Airport\Coen Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDQ60801/IDQ60801.94260.axf", "C:\\GetWeather\\Burketown Airport\\Burketown Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.94216.axf", "C:\\GetWeather\\Kununurra Airport\\Kununurra Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.95214.axf", "C:\\GetWeather\\Wyndham Airport\\Wyndham Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.94317.axf", "C:\\GetWeather\\Newman Airport\\Newman Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.95305.axf", "C:\\GetWeather\\Onslow Airport\\Onslow Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.94308.axf", "C:\\GetWeather\\Roebourne Airport\\Roebourne Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.99217.axf", "C:\\GetWeather\\Solomon Airport\\Solomon Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.94429.axf", "C:\\GetWeather\\Mount Magnet Airport\\Mount Magnet Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.95402.axf", "C:\\GetWeather\\Shark Bay Airport\\Shark Bay Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.94450.axf", "C:\\GetWeather\\Leonora Airport\\Leonora Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.94610.axf", "C:\\GetWeather\\Perth Airport\\Perth Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.95611.axf", "C:\\GetWeather\\Busselton Airport\\Busselton Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.94802.axf", "C:\\GetWeather\\Albany Airport\\Albany Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.95638.axf", "C:\\GetWeather\\Esperance Airport\\Esperance Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.95634.axf", "C:\\GetWeather\\Southern Cross Airport\\Southern Cross Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.95625.axf", "C:\\GetWeather\\Cunderdin Airport\\Cunderdin Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.94417.axf", "C:\\GetWeather\\Morawa Airport\\Morawa Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.94403.axf", "C:\\GetWeather\\Geraldton Airport\\Geraldton Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.95642.axf", "C:\\GetWeather\\Norseman Airport\\Norseman Airport.csv");
-            webClient.DownloadFile("http://www.bom.gov.au/fwo/IDW60801/IDW60801.95439.axf", "C:\\GetWeather\\Wiluna Airport\\Wiluna Airport.csv");
+            Airports Coen = new Airports
+            {
+                Name = "Coen Airport",
+                Url = "http://www.bom.gov.au/fwo/IDQ60801/IDQ60801.94183.axf"
+            };
+
+            Airports Burketown = new Airports
+            {
+                Name = "Burketown Airport",
+                Url = "http://www.bom.gov.au/fwo/IDQ60801/IDQ60801.94260.axf"
+            };
+
+            Airports Kununurra = new Airports
+            {
+                Name = "Kununurra Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.94216.axf"
+            };
+
+            Airports Wyndham = new Airports
+            {
+                Name = "Wyndham Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.95214.axf"
+            };
+
+            Airports Newman = new Airports
+            {
+                Name = "Newman Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.94317.axf"
+            };
+
+            Airports Onslow = new Airports
+            {
+                Name = "Onslow Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.95305.axf"
+            };
+
+            Airports Roebourne = new Airports
+            {
+                Name = "Roebourne Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.94308.axf"
+            };
+
+            Airports Soloman = new Airports
+            {
+                Name = "Soloman Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.99217.axf"
+            };
+
+            Airports MtMagnet= new Airports
+            {
+                Name = "Mount Magnet Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.94429.axf"
+            };
+
+            Airports SharkBay = new Airports
+            {
+                Name = "Shark Bay Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.95402.axf"
+            };
+
+            Airports Leonora = new Airports
+            {
+                Name = "Leonora Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.94450.axf"
+            };
+
+            Airports Perth = new Airports
+            {
+                Name = "Perth Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.94610.axf"
+            };
+
+            Airports Busselton = new Airports
+            {
+                Name = "Busselton Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.95611.axf"
+            };
+
+            Airports Albany = new Airports
+            {
+                Name = "Albany Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.94802.axf"
+            };
+
+            Airports Esperance = new Airports
+            {
+                Name = "Esperance Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.95638.axf"
+            };
+
+            Airports SouthernCross = new Airports
+            {
+                Name = "Southern Cross Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.95634.axf"
+            };
+
+            Airports Cunderdin = new Airports
+            {
+                Name = "Cunderdin Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.95625.axf"
+            };
+
+            Airports Morawa = new Airports
+            {
+                Name = "Morawa Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.94417.axf"
+            };
+
+            Airports Geraldton = new Airports
+            {
+                Name = "Geraldton Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.94403.axf"
+            };
+
+            Airports Norseman = new Airports
+            {
+                Name = "Norseman Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.95642.axf"
+            };
+
+            Airports Wiluna = new Airports
+            {
+                Name = "Wiluna Airport",
+                Url = "http://www.bom.gov.au/fwo/IDW60801/IDW60801.95439.axf"
+            };
 
         }        
     }
